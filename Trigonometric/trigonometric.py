fda9591134f621e24930bdd690918fe81aeddf45
Nooria <samihamaysoonnooria@gmail.com>
2025-03-09 14:11:45 +0600

    dfghjxcvbn

---
+++ Trigonometric/trigonometric.py
@@ -0,0 +1,14 @@
+import math
+
+print(math.ceil(45.12))
+print(math.floor(65.89))
+
+print(math.factorial(5))
+
+a = math.sin(45)
+b = math.cos(45)
+c = math.tan(45)
+
+print("value of sin(45)= ",a)
+print("value of cos(45)= ",b)
+print("value of tan(45)= ",c)
